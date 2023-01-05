9b4a9f4d5f204b8553bb316a79ab9cb09eb703f3
JAYASHRINIDHI <108124890+JAYASHRINIDHI@users.noreply.github.com>
2023-01-05 16:55:49 +0530

    Add files via upload

---
+++ text remove.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import keras_ocr
+import cv2
+import math
+import numpy as np
+import tensorflow as tf
+def midpoint(x1, y1, x2, y2):
+    x_mid = int((x1 + x2)/2)
+    y_mid = int((y1 + y2)/2)
+    return (x_mid, y_mid)
+pipeline = keras_ocr.pipeline.Pipeline()
+def inpaint_text(img_path, pipeline):
+    # read image
+    img = keras_ocr.tools.read('watermark.png')
+    # generate (word, box) tuples
+    prediction_groups = pipeline.recognize([img])
+    mask = np.zeros(img.shape[:2], dtype="uint8")
+    for box in prediction_groups[0]:
+        x0, y0 = box[1][0]
+        x1, y1 = box[1][1]
+        x2, y2 = box[1][2]
+        x3, y3 = box[1][3]
+       
+        x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
+        x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
+       
+        thickness = int(math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ))
+       
+        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255,    
+        thickness)
+        img = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS)
+                 
+    return(img)
+
+img_text_removed = inpaint_text('watermark.png', pipeline)
+
+plt.imshow(img_text_removed)
+
+cv2.imwrite('text_removed_image.jpg', cv2.cvtColor(img_text_removed, cv2.COLOR_BGR2RGB))
+
+
+# In[ ]:
+
+
+
+
